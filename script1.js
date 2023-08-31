2b4e9ffa27d8b0f1c3fb642d54810d12899d2460
unknown <christellamunezero01@gmail.com>
2023-08-31 05:40:57 -0700

    portfolio

---
+++ script1.js
@@ -0,0 +1,37 @@
+//----variables----//
+let firstName ="christella"
+let lastName ="munezero"
+let address ="main street,first street";
+console.log(firstName,lastName,address);
+//concanate//
+let street ="kirehe"
+let country ="rwanda"
+let fullMailingAddress ="kirehe "+"rwanda"
+console.log(fullMailingAddress);
+//--number//
+let score1 = 20;
+let score2 = 30;
+let score3 = 40;
+let totalScore =(score1+score2+score3);
+let averageScore =(totalScore/3);
+console.log(totalScore,averageScore)
+let plates =20;
+let people =7;
+let remainingplates =20 % 7;
+let add = remainingplates + 1;
+let mesage ="There are (your value goes here) plates available"
+console.log(mesage);
+
+//--Arrays---//
+l= ["avoca", "mango","apple",45];
+let firstFruits =fruits[0],
+fruits [3] = ['lemon']
+
+
+//
+
+
+
+
+
+
